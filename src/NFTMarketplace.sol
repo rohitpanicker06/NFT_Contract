27db8663b699bf3339b1a4e774196ccbd1ab21e1
NagendraBabuShakamuri <shakamuri.n@northeastern.edu>
2024-04-05 00:50:32 -0400

    Updated the constructor to take inputs.

--- src/NFTMarketplace.sol
+++ src/NFTMarketplace.sol
@@ -1,7 +1,6 @@
 pragma solidity ^0.8.13;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "../src/Counters.sol";
 
 contract NFTMarketplace is ERC721URIStorage {
@@ -10,7 +9,7 @@ contract NFTMarketplace is ERC721URIStorage {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
-    constructor() ERC721("NFTMarketPlace","NFTM"){
+    constructor(string memory _name, string memory _symbol) ERC721(_name, _symbol){
         owner = msg.sender;
     }
 
